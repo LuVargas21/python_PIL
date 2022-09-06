746f73d90ea6e24fe054614812d91165e9c1f662
LuVargas21 <96500749+LuVargas21@users.noreply.github.com>
2022-09-06 19:18:34 -0300

    Create ejercitacion_lm.py

---
+++ ejercitacion_lm.py
@@ -0,0 +1,34 @@
+
+# <<< ARRAYS >>>
+
+#arreglo = ["banana", "pera", "aguacate", "sandia", "mandarina", "manzana", "frutilla"]
+
+#for fruta in arreglo:
+ #   if fruta.endswith("a"): el endswith  muestra todas las palabras que terminen con la letra "a" por ejemplo
+  #    print(fruta)
+
+
+#for fruta in arreglo:
+ #  if fruta == "sandia":   #muestra  los elementos del array hasta sandia.
+  #   break
+   # print (fruta)
+
+#arreglo.reverse() muestra los elementos de atras hacia adelante
+
+#arreglo.remove("aguacate") elimina un elemento dentro del array 
+# arreglo.append("kiwi") permite agrear un elemento
+#for fruta in arreglo:
+ # print(fruta)
+
+# en este ejemplo muestra como recorrer cada elemento "letra" del array "texto"
+#texto= "hola mundo"
+#for letra in texto:
+ #   print(letra)
+
+#for numero in range(10):  #range permite recorrer un rango de numeros. Se puede usar (4, 10) num minimo y maximo de un rango
+ #   if (numero > 5):    # con el if puedo establecer una condicion dentro de ese rango, por ej numeros mayores a 5
+  #   print(numero)
+
+
+#Encripar y desencriptar archivos
+
